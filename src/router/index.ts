9d5b10b1c1bfa707e192d30aa1e9573ffcc4441a
JieGeng <2712788035@qq.com>
2023-05-24 12:31:57 +0800

    封装了下nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 // createWebHashHistory hash模式
 // createWebHistory history模式
 import routes from './routes';
+// 引入封装了一下的进度条
+import { start, done } from '@/utils/NProgress';
 
 // 创建路由器
 const router = createRouter({
@@ -16,6 +18,18 @@ const router = createRouter({
             top: 0
         }
     }
+})
+
+// 全局前置守卫
+router.beforeEach(() => {
+    start();
+    // 如果什么都没有，undefined 或返回 true，则导航是有效的，并调用下一个导航守卫
+    return true;
+})
+
+// 全局后置钩子
+router.afterEach(() => {
+    done();
 })
 
 export default router;
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,8 @@
 // 进行axios二次封装：使用请求与相应拦截
 import axios from 'axios'
-import { ElMessage } from 'element-plus';
+import { ElMessage } from 'element-plus'
+// 引入封装了一下的进度条
+import { start, done } from '@/utils/NProgress';
 
 // 利用axios对象的create方法，去创建axios实例（其它配置：基础路径、超时的时间）
 const request = axios.create({
@@ -11,6 +13,7 @@ const request = axios.create({
 
 // 第二步：request实例添加请求与相应拦截器
 request.interceptors.request.use((config) => {
+    start();
     // config配置对象，headers属性请求头，经常给服务器端，携带公共参数
     // 返回配置对象
     return config;
@@ -18,6 +21,7 @@ request.interceptors.request.use((config) => {
 
 // 第三步
 request.interceptors.response.use((response) => {
+    done();
     // 成功回调
     // 简化数据
     return response.data;
