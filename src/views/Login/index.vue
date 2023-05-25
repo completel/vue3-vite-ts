df31e0002f95718a7c6dd2e5dc4ab24d20181132
jieG <2712788035@qq.com>
2023-05-25 22:08:08 +0800

    表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,13 +3,13 @@
         <el-row>
             <el-col :span="12" :xs="0"></el-col>
             <el-col :span="12" :xs="24">
-                <el-form class="login_form">
+                <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
                     <h1>Hello</h1>
                     <h2>欢迎来到这里~</h2>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="password">
                         <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password"
                             :show-password="true"></el-input>
                     </el-form-item>
@@ -27,7 +27,7 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { ElNotification } from 'element-plus'
+import { ElNotification, FormRules } from 'element-plus'
 import useUserStore from '@/sotre/module/user'
 import { User, Lock } from '@element-plus/icons-vue'
 import { getTime } from '@/utils/time'
@@ -42,8 +42,13 @@ const useStore = useUserStore()
 const $router = useRouter()
 // 定义变量控制按钮加载效果
 const loading = ref(false)
+// 获取el-form组件
+const loginForms = ref()
 
+// 登录按钮的回调
 const login = async () => {
+    // 保证全部的表单项校验通过再发请求
+    await loginForms.value.validate()
     // 开始加载
     loading.value = true
     // 点击登录需要做什么？
@@ -74,6 +79,44 @@ const login = async () => {
     }
 
 }
+
+// 自定义校验规则函数
+const validatorUserName = (_: any, value: any, callback: any) => {
+    // rule：即为校验规则对象
+    // value：即为表单元素文本内容
+    // callback：如果符合条件callback放行通过
+    // 如果不符合条件callback方法，注入错误提示信息
+    if (value.length >= 5) {
+        callback()
+    } else {
+        callback(new Error('账号长度至少五位'))
+    }
+}
+const validatorPassword = (_: any, value: any, callback: any) => {
+    if (value.length >= 6) {
+        callback()
+    } else {
+        callback(new Error('密码长度至少六位'))
+    }
+}
+
+// 定义表单校验需要配置的对象 FormRules 表单验证是类型，有提示
+const rules = reactive<FormRules>({
+    // 规则对象属性：required：代表这个字段务必要校验
+    // min：文本长度至少多少位
+    // max：文本长度最多多少位
+    // message：错误提示信息
+    // trigger：触发校验表单的时机 change->文本发生变化出发校验，blur：失去焦点时触发
+    username: [
+        // { required: true, message: '用户名不能为空', trigger: 'blur' },
+        // { required: true, min: 4, max: 10, message: '账号长度至少6位', trigger: 'change' }
+        { trigger: 'change', validator: validatorUserName }
+    ],
+    password: [
+        // { required: true, min: 6, max: 15, message: '密码至少6位', trigger: 'change' }
+        { trigger: 'change', validator: validatorPassword }
+    ]
+})
 
 </script>
 
