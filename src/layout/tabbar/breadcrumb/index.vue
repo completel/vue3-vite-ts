e0677bd578f9400f1ab1e20acb6f46dd48471866
jieG <2712788035@qq.com>
2023-05-29 21:57:48 +0800

    修复面包屑el-icon，控制台警告问题

--- src/layout/tabbar/breadcrumb/index.vue
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -7,9 +7,19 @@
     <el-breadcrumb separator-icon="ArrowRight">
         <!-- 面包屑需要展示路由名字和标题 -->
         <el-breadcrumb-item v-for="(item, index) in $route.matched" :key="index" v-show="item.meta.title" :to="item.path">
-            <!-- 图标 -->
-            <el-icon style="vertical-align: top;">
-                <component :is="item.meta.icon"></component>
+            <el-icon>
+                <!-- 
+                    \// title: '',
+                    \// icon: ''
+                    在路由器配置项中，将title和icon注释掉，控制台就不会报错了
+                    大概原因应该是：v-show只是把layout组件通过display: none; 隐藏了
+                    但是图标组件还是去找meta下的icon，所以抛出警告
+                    index.vue:19 [Vue warn]: Invalid vnode type when creating vnode: . 
+                    at <ElIcon> 
+                    解决办法1：在路由器配置项中将icon注释掉
+                    解决办法2：在component中加上v-if，如果meta.icon为空，就删除这个标签
+                 -->
+                <component v-if="item.meta.icon" :is="item.meta.icon"></component>
             </el-icon>
             <!-- 面包屑展示匹配路由的标题 -->
             <span style="margin: 0 5px;">{{ item.meta.title }}</span>
@@ -41,4 +51,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-icon {
+    vertical-align: top;
+}
+</style>
