19efb8b30c4743172dd02cc61c5c51fb7c3d17a1
jieG <2712788035@qq.com>
2023-06-30 22:07:08 +0800

    完成权限管理模块

--- src/views/acl/permission/index.vue
+++ src/views/acl/permission/index.vue
@@ -1,8 +1,126 @@
 <template>
-    <h1>菜单管理</h1>
+    <div>
+        <el-table :data="PermisstionArr" stripe style="width: 100%; margin-bottom: 20px" row-key="id" border>
+            <el-table-column prop="name" label="名称" />
+            <el-table-column prop="code" label="权限值" />
+            <el-table-column prop="updateTime" label="修改时间" />
+            <el-table-column label="操作">
+                <!-- row就是每一个菜单的按钮对象 -->
+                <template #="{ row }">
+                    <el-button type="primary" :disabled="row.level == 4 ? true : false" @click="addPermisstion(row)">{{
+                        row.level
+                        == 3 ? "添加功能" : "添加菜单"
+                    }}</el-button>
+                    <el-button type="success" :disabled="row.level == 1 ? true : false"
+                        @click="updatePermisstion(row)">编辑</el-button>
+                    <el-popconfirm :title="`你确定要删除${row.name}吗？`" width="260px" @confirm="removeMenu(row.id)">
+                        <template #reference>
+                            <el-button type="danger" :disabled="row.level == 1 ? true : false">删除</el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
+        <!-- 对话框组件:添加或者更新已有的菜单属性 -->
+        <el-dialog v-model="dialogVisible" :title="menuData.id ? '更新菜单' : '添加菜单'">
+            <!-- 表单组件：收集新增或已有的菜单数据 -->
+            <el-form label-width="60px">
+                <el-form-item label="名称">
+                    <el-input placeholder="请你输入菜单名称" v-model="menuData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="权限值">
+                    <el-input placeholder="请你输入权限值" v-model="menuData.code"></el-input>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="save">
+                        确定
+                    </el-button>
+                </span>
+            </template>
+        </el-dialog>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, reactive } from 'vue'
+import { reqAllPermisstion, reqAddOrUpdateMenu, reqRemoveMenu } from '@/api/acl/menu'
+import type { PermisstionResponseData, PermisstionList, Permisstion, MenuParams } from '@/api/acl/menu/type'
+import { ElMessage } from 'element-plus';
+
+// 存储菜单数据
+const PermisstionArr = ref<PermisstionList>([])
+// 控制对话框显示隐藏
+const dialogVisible = ref<boolean>(false)
+// 携带的参数
+const menuData = reactive<MenuParams>({
+    code: '',
+    level: 0,
+    name: '',
+    pid: 0
+})
+
+// 获取菜单数据的方法
+const getHasPermisstion = async () => {
+    const result: PermisstionResponseData = await reqAllPermisstion()
+    if (result.code == 200) {
+        PermisstionArr.value = result.data
+    }
+}
+// 添加菜单按钮的回调
+const addPermisstion = (row: Permisstion) => {
+    // 清空数据
+    Object.assign(menuData, {
+        id: 0,
+        code: '',
+        level: 0,
+        name: '',
+        pid: 0
+    })
+    dialogVisible.value = true
+    // 收集新增的菜单的level数值
+    menuData.level = row.level + 1
+    // 给谁新增子菜单
+    menuData.pid = row.id as number
+}
+// 编辑已有的菜单
+const updatePermisstion = (row: Permisstion) => {
+    dialogVisible.value = true
+    // 收集已有的菜单数据进行更新
+    Object.assign(menuData, row)
+}
+// 确定按钮的回调
+const save = async () => {
+    const result = await reqAddOrUpdateMenu(menuData)
+    if (result.code == 200) {
+        // 对话框隐藏
+        dialogVisible.value = false
+        // 提示信息
+        ElMessage({
+            type: 'success',
+            message: menuData.id ? '更新成功' : '添加成功'
+        })
+        getHasPermisstion()
+    }
+
+}
+// 删除按钮回调
+const removeMenu = async (id: number) => {
+    const result = await reqRemoveMenu(id)
+    if (result.code == 200) {
+        ElMessage({
+            type: 'success',
+            message: '删除成功'
+        })
+        getHasPermisstion()
+    }
+}
+
+onMounted(() => {
+    getHasPermisstion()
+})
 
 </script>
 
--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -1,8 +1,258 @@
 <template>
-    <h1>角色管理</h1>
+    <div>
+        <el-card>
+            <el-form :inline="true">
+                <el-form-item label="职位搜索">
+                    <el-input placeholder="请你输入搜索职位的关键字" v-model="keyword"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" :disabled="!keyword" @click="serach">搜索</el-button>
+                    <el-button type="success" @click="reset">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+        <el-card style="margin: 10px 0">
+            <el-button type="primary" icon="Plus" @click="addRole">添加职位</el-button>
+            <el-table border style="margin: 10px 0" :data="allRole">
+                <el-table-column type="index" align="center" label="#"></el-table-column>
+                <el-table-column label="ID" align="center" prop="id"></el-table-column>
+                <el-table-column label="职位名称" align="center" show-overflow-tooltop prop="roleName"></el-table-column>
+                <el-table-column label="创建时间" align="center" show-overflow-tooltop prop="createTime"></el-table-column>
+                <el-table-column label="更新时间" align="center" show-overflow-tooltop prop="updateTime"></el-table-column>
+                <el-table-column label="操作" align="center">
+                    <!-- row：已有的职位对象 -->
+                    <template #="{ row }">
+                        <el-button type="primary" size="small" icon="User" @click="setPermisstion(row)">分配权限</el-button>
+                        <el-button type="success" size="small" icon="Edit" @click="updateRole(row)">编辑</el-button>
+                        <el-popconfirm :title="`你确定要删除${row.roleName}？`" width="260px" @confirm="removeRole(row.id)">
+                            <template #reference>
+                                <el-button type="danger" size="small" icon="Delete">删除</el-button>
+                            </template>
+                        </el-popconfirm>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[10, 20, 30, 40]"
+                :background="true" layout="prev, pager, next, jumper, ->, total, sizes" :total="total"
+                @current-change="getHasRole" @size-change="sizeChange" />
+        </el-card>
+        <!-- 添加职位与更新已有职位的结构：对话框 -->
+        <el-dialog v-model="dialogVisite" :title="RoleParams.id ? '更新职位' : '添加职位'">
+            <el-form :model="RoleParams" :rules="rules" ref="form">
+                <el-form-item label="职位名称" prop="roleName">
+                    <el-input placeholder="请你输入职位名称~" v-model="RoleParams.roleName"></el-input>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisite = false">取消</el-button>
+                    <el-button type="primary" @click="save">确定</el-button>
+                </span>
+            </template>
+        </el-dialog>
+        <!-- 抽屉组件：分配用户的职位菜单权限与按钮权限 -->
+        <el-drawer v-model="drawer">
+            <template #header>
+                <h4>分配权限</h4>
+            </template>
+            <template #default>
+                <div>
+                    <el-tree ref="tree" :data="menuArr" show-checkbox node-key="id" default-expand-all
+                        :default-checked-keys="selectArr" :props="defaultProps" />
+                </div>
+            </template>
+            <template #footer>
+                <div style="flex: auto">
+                    <el-button @click="drawer = false">取消</el-button>
+                    <el-button type="primary" @click="handler">确定</el-button>
+                </div>
+            </template>
+        </el-drawer>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, reactive, nextTick } from 'vue'
+import { ElMessage, FormRules } from 'element-plus'
+import { reqAllRoleList, reqAddOrUpdate, reqAllMenuList, reqSetPerMisstion, reqRemoveRole } from '@/api/acl/role'
+// 引入骨架仓库
+import useLayOutSettingStore from '@/store/module/settins'
+import type { RoleResponseData, Records, RoleData, MenuList } from '@/api/acl/role/type'
+
+// 获取仓库
+const settingStore = useLayOutSettingStore()
+// 当前页码
+const pageNo = ref<number>(1)
+// 一夜展示几条数据
+const pageSize = ref<number>(10)
+// 搜索职位关键字
+const keyword = ref<string>('')
+// 存储全部已有的职位
+const allRole = ref<Records>([])
+// 职位总个数
+const total = ref<number>(0)
+// 控制对话框的显示与隐藏
+const dialogVisite = ref<boolean>(false)
+// 收集新增岗位数据
+const RoleParams = reactive<RoleData>({
+    roleName: ''
+})
+// 获取form组件实例
+const form = ref<any>()
+// 抽屉显示与隐藏
+const drawer = ref<boolean>(false)
+// 定义数据存储用户权限的数据
+const menuArr = ref<MenuList>([])
+// 准备一个数组：数组用于存储勾选的节点的id（四级）
+const selectArr = ref<number[]>([])
+// 树形结构ref
+const tree = ref()
+// 树形结构
+const defaultProps = {
+    children: 'children',
+    label: 'name',
+}
+
+// 获取全部用户信息的方法|分页器当前页码发生变化的回调
+const getHasRole = async (pager = 1) => {
+    // 修改当前页码
+    pageNo.value = pager
+    const result: RoleResponseData = await reqAllRoleList(pageNo.value, pageSize.value, keyword.value)
+    if (result.code == 200) {
+        total.value = result.data.total
+        allRole.value = result.data.records
+    }
+}
+// 下拉菜单
+const sizeChange = () => {
+    getHasRole()
+}
+// 搜索按钮回调
+const serach = () => {
+    // 再次发送请求根据关键字
+    getHasRole()
+}
+// 重置按钮回调
+const reset = () => {
+    settingStore.refsh = !settingStore.refsh
+}
+// 添加职位按钮回调
+const addRole = () => {
+    dialogVisite.value = true
+    // 清空数据
+    Object.assign(RoleParams, {
+        roleName: '',
+        id: 0
+    })
+    // 清空上一次表单错误情况
+    nextTick(() => {
+        form.value.clearValidate('roleName')
+    })
+}
+// 更新已有职位按钮的回调
+const updateRole = (row: RoleData) => {
+    // 显示出对话框
+    dialogVisite.value = true
+    // 存储已有的职位带有ID
+    Object.assign(RoleParams, row)
+    // 清空上一次表单错误情况
+    nextTick(() => {
+        form.value.clearValidate('roleName')
+    })
+}
+const validatorRoleName = (_: any, value: string, callback: any) => {
+    if (value.trim().length >= 2) {
+        callback()
+    } else {
+        callback(new Error('职位名称必须大于或等于2位'))
+    }
+}
+// 职位校验规则
+const rules: FormRules = {
+    roleName: [
+        { required: true, trigger: 'blur', validator: validatorRoleName }
+    ]
+}
+// 对话框确定按钮回调
+const save = async () => {
+    // 表单校验规则，结果通过再发请求，没有通过不用发送请求
+    await form.value.validate()
+    // 添加职位|或者更新职位的请求
+    const result = await reqAddOrUpdate(RoleParams)
+    if (result.code == 200) {
+        // 提示信息
+        ElMessage({
+            type: 'success',
+            message: RoleParams.id ? '更新成功' : '添加成功'
+        })
+        // 对话框关闭
+        dialogVisite.value = false
+        // 再次更新已有的职位
+        getHasRole(RoleParams.id ? pageNo.value : 1)
+    }
+}
+// 分配权限按钮回调
+const setPermisstion = async (row: RoleData) => {
+    // 抽屉显示出来
+    drawer.value = true
+    // 收集当前要分配权限职位的数据
+    Object.assign(RoleParams, row)
+    // 更具职位获取权限数据
+    const result = await reqAllMenuList(RoleParams.id as number)
+    if (result.code == 200) {
+        menuArr.value = result.data
+        selectArr.value = filterSelectArr(menuArr.value, [])
+    }
+}
+const filterSelectArr = (allData: MenuList, initArr: number[]) => {
+    allData.forEach(item => {
+        if (item.select && item.level === 4) {
+            initArr.push(item.id)
+        }
+        if (item.children && item.children.length > 0) {
+            filterSelectArr(item.children, initArr)
+        }
+    })
+    return initArr
+}
+// 抽屉确定按钮回调
+const handler = async () => {
+    // 职位的id
+    const roleId = RoleParams.id as number
+    // 选中的id
+    const arr = tree.value.getCheckedKeys()
+    // 半选的id
+    const arr1 = tree.value.getHalfCheckedKeys()
+    const permissionId = arr.concat(arr1)
+    // 下发权限
+    const result = await reqSetPerMisstion(roleId, permissionId)
+    if (result.code == 200) {
+        // 抽屉关闭
+        drawer.value = false
+        ElMessage({
+            type: 'success',
+            message: '分配权限成功'
+        })
+        // 页面刷新
+        window.location.reload()
+    }
+}
+// 删除已有的职位
+const removeRole = async (id: number) => {
+    const result = await reqRemoveRole(id)
+    if (result.code == 200) {
+        ElMessage({
+            type: 'success',
+            message: '删除职位成功'
+        })
+        getHasRole(allRole.value.length > 1 ? pageNo.value : pageNo.value - 1)
+    }
+}
+
+onMounted(() => {
+    getHasRole()
+})
+
 
 </script>
 
